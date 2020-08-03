d5b1abf95f2f4e5e358b5d99997ee4a60e937811
DooDac LongNhat <mister.postmodern@gmail.com>
2020-08-03 22:04:53 +0300

    some components added, basic markup and styles added, snap functional added, useref added

---
+++ src/components/Videos/Video.jsx
@@ -0,0 +1,32 @@
+import React, {useRef, useState} from "react"
+import '../../Video.css'
+
+function Video() {
+  const [playing, setPlay] = useState(false)
+  const videoRef = useRef(null)
+  const onVideoPress = () => {
+    if(playing) {
+      videoRef.current.pause()
+      setPlay(false)
+    } else {
+      videoRef.current.play()
+      setPlay(true)
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        loop
+        onClick={onVideoPress}
+        ref={videoRef}
+        src="https://v16m.tiktokcdn.com/0fd54de9269eca22423e4eef4bc31026/5f2afada/video/tos/useast2a/tos-useast2a-ve-0068c003/63cafdbf013245f2a56d0ff2607ba804/?a=1233&br=6076&bt=3038&cr=0&cs=0&dr=0&ds=3&er=&l=202008031830490101901860143235CA94&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=am9uOzd4bjdodjMzZjczM0ApM2ZoNzw1ZmQ2NztkZjNlZWdzbDJwYi1kcjJfLS01MTZzcy5gYGNiNC1jMC00MWA1NTM6Yw%3D%3D&vl=&vr=">
+      </video>
+    </div>
+  )
+}
+
+export {
+  Video
+}
