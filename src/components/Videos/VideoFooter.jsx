1b6849400777f57e9f7f68f5ed7a46322b09964c
DooDac LongNhat <mister.postmodern@gmail.com>
2020-08-04 14:31:16 +0300

    videofooter added, ticker line added, footer styles and shit

---
+++ src/components/Videos/VideoFooter.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from "react-ticker"
+
+function VideoFooter({channel, description, song}) {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@{channel}</h3>
+        <p>{description}</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon"/>
+          <Ticker mode="smooth">
+            {
+              ({}) => (
+                <>
+                  <p>{song}</p>
+                </>
+              )
+            }
+          </Ticker>
+        </div>
+      </div>
+      <img className="videoFooter__record" src="https://image.flaticon.com/icons/svg/72/72216.svg" alt=""/>
+
+    </div>
+  )
+}
+
+export {
+  VideoFooter
+}
